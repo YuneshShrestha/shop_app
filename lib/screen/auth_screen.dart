922d10fe67ee85756daec4d3bf3119519e713edc
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2022-12-30 18:53:33 +0545

    AnimatedContainer

--- lib/screen/auth_screen.dart
+++ lib/screen/auth_screen.dart
@@ -192,12 +192,12 @@ class _AuthCardState extends State<AuthCard>
     if (_authMode == AuthMode.login) {
       setState(() {
         _authMode = AuthMode.signUp;
-        _animationController!.forward();
+        // _animationController!.forward();
       });
     } else {
       setState(() {
         _authMode = AuthMode.login;
-        _animationController!.reverse();
+        // _animationController!.reverse();
       });
     }
   }
@@ -210,7 +210,16 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
+      child: AnimatedContainer(
+        duration:const Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.signUp ? 320 : 260,
+        // height: _heightAnimation!.value.height,
+        constraints: BoxConstraints(
+          minHeight: _authMode == AuthMode.signUp ? 320 : 260,
+        ),
+        width: deviceSize.width * 0.75,
+        padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -293,18 +302,6 @@ class _AuthCardState extends State<AuthCard>
             ),
           ),
         ),
-        builder: (ctx, child) {
-          return Container(
-            // height: _authMode == AuthMode.signUp ? 320 : 260,
-            height: _heightAnimation!.value.height,
-            constraints:
-                BoxConstraints(minHeight: _heightAnimation!.value.height),
-            width: deviceSize.width * 0.75,
-            padding: const EdgeInsets.all(16.0),
-            child: child,
-          );
-        },
-        animation: _heightAnimation!,
       ),
     );
   }
